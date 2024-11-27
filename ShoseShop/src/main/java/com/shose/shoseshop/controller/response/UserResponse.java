46bb49cd6618149d31d76e637c18bb89a861a716
hai <vhai31102002@gmail.com>
2024-11-27 22:57:58 +0700

    update user response

--- ShoseShop/src/main/java/com/shose/shoseshop/controller/response/UserResponse.java
+++ ShoseShop/src/main/java/com/shose/shoseshop/controller/response/UserResponse.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponse extends BaseEntity {
+    private Long id;
     private String username;
     private String password;
     private String firstName;
